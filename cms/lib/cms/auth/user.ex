7efbae723f7b91811b553ea547ab47a94c82bd34
Ben Hesseldieck <b.hesseldieck@gmail.com>
2019-07-11 12:04:47 +0200

    setup postgres

---
+++ cms/lib/cms/auth/user.ex
@@ -0,0 +1,20 @@
+defmodule Cms.Auth.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :admin, :boolean, default: false
+    field :email, :string
+    field :name, :string
+    field :password, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :password, :admin])
+    |> validate_required([:name, :email, :password, :admin])
+  end
+end
