5a6f4bced05c8193aa9d8595ab9a2e34fc19f17a
Ben Hesseldieck <b.hesseldieck@gmail.com>
2019-07-17 18:32:42 +0200

    WIP adding credentials to account context

---
+++ cms/lib/cms/accounts/credential.ex
@@ -0,0 +1,19 @@
+defmodule Cms.Accounts.Credential do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "credentials" do
+    field :email, :string
+    field :user_id, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(credential, attrs) do
+    credential
+    |> cast(attrs, [:email])
+    |> validate_required([:email])
+    |> unique_constraint(:email)
+  end
+end
